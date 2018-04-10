489fd616d0c64d0ce627512e04f20dc618642c1f
Preety Singh <singh_pt@India.XoriantCorp.com>
2018-04-10 15:50:44 +0530

    test

--- balance-transfer/artifacts/src/github.com/loanApp/loanApp.go
+++ balance-transfer/artifacts/src/github.com/loanApp/loanApp.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
-	"github.com/jasonlvhit/gocron"
+	"github.com/hyperledger/jasonlvhit/gocron"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
 )
